238a9f029085c3a318bd89ad8d6fed6cd9094923
dust1nmax <dust1nmax0712@gmail.com>
2024-08-10 21:48:31 +0800

    Add files via upload

--- PlatformIO/Projects/text/src/main.cpp
+++ PlatformIO/Projects/text/src/main.cpp
@@ -1,12 +1,26 @@
 #include <Arduino.h>
+#include <micro_ros_platformio.h>
+#include <rcl/rcl.h>
+#include <rclc/rclc.h>
+#include <rclc/executor.h>
+
+rcl_allocator_t allocator;
+rclc_support_t support;
+rcl_node_t node;
+rclc_executor_t executor;
 
 void setup() {
   Serial.begin(115200);
-  Serial.printf("hello world setup\n");
+  set_microros_serial_transports(Serial); //确认 micro-ros 使用串口通信
+  delay(2000);
+  allocator = rcl_get_default_allocator();
+  rclc_support_init(&support, 0, NULL, &allocator);
+  rclc_node_init_default(&node, "hello_mocroros", "", &support);
+  rclc_executor_init(&executor, &support.context, 1, &allocator);
 }
 
 void loop() {
-  Serial.printf("hello world loop\n");
-  delay(1000);
+  delay(100);
+  rclc_executor_spin_some(&executor,RCL_MS_TO_NS(100));
 }
 
